ba71f8ea728e1d4c232763991d5370589ce34b32
Jennifer Chen <cc2732@gmail.com>
2014-08-06 16:01:04 -0700

    Fix card show

--- app/assets/javascripts/views/cards/card_show.js
+++ app/assets/javascripts/views/cards/card_show.js
@@ -2,20 +2,10 @@ TrelloClone.Views.CardShow = Backbone.CompositeView.extend({
   template: JST['cards/show'],
   tagName: 'li',
   className: "card panel panel-default",
-  events: {
-    "click .launch-card-detail": 'viewDetail'
-  },
   id: function(){ return "card-" + this.model.id },
   render: function(){
     var renderedContent = this.template({ card: this.model });
     this.$el.html(renderedContent);
     return this;
-  },
-  viewDetail: function(event){
-    var cardDetailView = new TrelloClone.Views.CardDetail({ model: this.model });
-    this.addSubview(".card-detail",cardDetailView);
-    var idTag = "#target-" + this.model.id;
-    this.$el.find(idTag).modal();
   }
-
 });
